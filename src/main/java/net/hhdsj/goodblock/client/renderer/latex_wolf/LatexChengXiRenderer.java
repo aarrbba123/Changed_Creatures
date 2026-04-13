67eb68a6552fb2b7eed522fcf9f663b9dfa1b391
hhdsjgit <abc18688567431@163.com>
2026-04-13 23:09:12 +0800

    Fixed Data configuration is overwriting Changed's entity accessories. #13

---
+++ src/main/java/net/hhdsj/goodblock/client/renderer/latex_wolf/LatexChengXiRenderer.java
@@ -0,0 +1,37 @@
+package net.hhdsj.goodblock.client.renderer.latex_wolf;
+
+import net.hhdsj.goodblock.client.model.latex_wolf.ModelLatexChengXi;
+import net.hhdsj.goodblock.entity.simple.LatexChengXiEntity;
+import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfModel;
+import net.ltxprogrammer.changed.util.Color3;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+
+public class LatexChengXiRenderer extends
+        AdvancedHumanoidRenderer<LatexChengXiEntity, ModelLatexChengXi> {
+    public LatexChengXiRenderer(EntityRendererProvider.Context context) {
+
+        super(context, new ModelLatexChengXi(context.bakeLayer(ModelLatexChengXi.LAYER_LOCATION)),
+                ArmorLatexMaleWolfModel.MODEL_SET,
+                0.5f);
+        this.addLayer(new LatexParticlesLayer<>(this, this.getModel()));
+        this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
+        this.addLayer(new CustomEyesLayer<>(this,
+                context.getModelSet(),
+                CustomEyesLayer::scleraColor,
+                CustomEyesLayer.fixedColorGlowing(Color3.parseHex("#fef19b")),
+                CustomEyesLayer.fixedColorGlowing(Color3.parseHex("#fef19b")),
+                CustomEyesLayer::noRender, CustomEyesLayer::noRender));
+        this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(LatexChengXiEntity entity) {
+        return new ResourceLocation("goodblock:textures/entities/latex/chengxi.png");
+    }
+}

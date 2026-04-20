f2eb43934dcc0bb4e20455394bbb95beeb0bbc9b
hhdsjgit <abc18688567431@163.com>
2026-04-20 22:49:47 +0800

    Add Entity Drop

--- src/main/java/net/hhdsj/goodblock/mixin/testmixin.java
+++
@@ -1,9 +0,0 @@
-package net.hhdsj.goodblock.mixin;
-
-import net.minecraft.server.level.ChunkHolder;
-import net.minecraft.server.level.ChunkMap;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ChunkMap.class)
-public class testmixin {
-}

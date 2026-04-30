e2c04498e4bc78df5a7c741d273087933a5417cb
hhdsjgit <abc18688567431@163.com>
2026-04-30 22:45:41 +0800

    Added new latex LatexDivineSwordKitsuneEntity

---
+++ src/main/java/net/hhdsj/goodblock/client/renderer/latex_fox/LatexDivineSwordKitsuneRenderer.java
@@ -0,0 +1,26 @@
+package net.hhdsj.goodblock.client.renderer.latex_fox;
+
+import net.hhdsj.goodblock.client.model.latex_wolf.ModelLatexDivineSwordKitsune;
+import net.hhdsj.goodblock.entity.simple.LatexDivineSwordKitsuneEntity;
+import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
+import net.ltxprogrammer.changed.client.renderer.layers.*;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfModel;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+public class LatexDivineSwordKitsuneRenderer extends AdvancedHumanoidRenderer<LatexDivineSwordKitsuneEntity, ModelLatexDivineSwordKitsune> {
+    public LatexDivineSwordKitsuneRenderer(EntityRendererProvider.Context context) {
+        super(context, new ModelLatexDivineSwordKitsune(context.bakeLayer(ModelLatexDivineSwordKitsune.LAYER_LOCATION)), ArmorLatexMaleWolfModel.MODEL_SET, 0.5f);
+        this.addLayer(new LatexParticlesLayer<>(this, getModel()));
+        this.addLayer(new CustomEyesLayer<>(this, context.getModelSet(), CustomEyesLayer::scleraColor, CustomEyesLayer::glowingIrisColorLeft, CustomEyesLayer::glowingIrisColorRight, CustomEyesLayer::noRender, CustomEyesLayer::noRender));
+        this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
+        this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
+    }
+
+    @Override
+    public @NotNull ResourceLocation getTextureLocation(@NotNull LatexDivineSwordKitsuneEntity entity) {
+        return new ResourceLocation("goodblock:textures/entities/latex/latex_divine_sword_kitsune.png");
+    }
+}
---
+++ src/main/java/net/hhdsj/goodblock/entity/simple/LatexDivineSwordKitsuneEntity.java
@@ -0,0 +1,36 @@
+package net.hhdsj.goodblock.entity.simple;
+
+import net.foxyas.changedaddon.entity.advanced.AbstractKitsuneEntity;
+import net.foxyas.changedaddon.init.ChangedAddonEntities;
+import net.hhdsj.goodblock.init.GoodblockModEntities;
+import net.ltxprogrammer.changed.entity.ChangedEntity;
+import net.ltxprogrammer.changed.entity.Gender;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.network.PlayMessages;
+
+import java.util.Objects;
+
+public class LatexDivineSwordKitsuneEntity extends AbstractKitsuneEntity {
+    @Override
+    protected void setAttributes(AttributeMap attributes) {
+        super.setAttributes(attributes);
+        Objects.requireNonNull(attributes.getInstance(Attributes.MOVEMENT_SPEED)).setBaseValue(1.1);
+        Objects.requireNonNull(attributes.getInstance(ForgeMod.SWIM_SPEED.get())).setBaseValue(1.1);
+    }
+
+    public LatexDivineSwordKitsuneEntity(EntityType<? extends ChangedEntity> type, Level level) {
+        super(type, level);
+    }
+
+    public LatexDivineSwordKitsuneEntity(PlayMessages.SpawnEntity ignoredPacket, Level world) {
+        this(GoodblockModEntities.LATEX_DIVINE_SWORD_KITSUNE.get(), world);
+    }
+
+    public Gender getGender() {
+        return Gender.MALE;
+    }
+}

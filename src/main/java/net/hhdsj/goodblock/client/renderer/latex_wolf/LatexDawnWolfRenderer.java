5f8dd6b6cbbed90e3a465898c2ee06f2366d4365
hhdsjgit <abc18688567431@163.com>
2026-04-30 22:44:00 +0800

    Added new latex LatexDawnWolfEntity

---
+++ src/main/java/net/hhdsj/goodblock/client/renderer/latex_wolf/LatexDawnWolfRenderer.java
@@ -0,0 +1,37 @@
+package net.hhdsj.goodblock.client.renderer.latex_wolf;
+
+import net.hhdsj.goodblock.client.model.latex_wolf.ModelLatexDawnWolf;
+import net.hhdsj.goodblock.entity.simple.LatexDawnWolfEntity;
+import net.ltxprogrammer.changed.client.renderer.AdvancedHumanoidRenderer;
+import net.ltxprogrammer.changed.client.renderer.layers.CustomEyesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.GasMaskLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.LatexParticlesLayer;
+import net.ltxprogrammer.changed.client.renderer.layers.TransfurCapeLayer;
+import net.ltxprogrammer.changed.client.renderer.model.armor.ArmorLatexMaleWolfModel;
+import net.ltxprogrammer.changed.util.Color3;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+
+public class LatexDawnWolfRenderer extends
+        AdvancedHumanoidRenderer<LatexDawnWolfEntity, ModelLatexDawnWolf> {
+    public LatexDawnWolfRenderer(EntityRendererProvider.Context context) {
+
+        super(context, new ModelLatexDawnWolf(context.bakeLayer(ModelLatexDawnWolf.LAYER_LOCATION)),
+                ArmorLatexMaleWolfModel.MODEL_SET,
+                0.5f);
+        this.addLayer(new LatexParticlesLayer<>(this, this.getModel()));
+        this.addLayer(TransfurCapeLayer.normalCape(this, context.getModelSet()));
+        this.addLayer(new CustomEyesLayer<>(this,
+                context.getModelSet(),
+                CustomEyesLayer::scleraColor,
+                CustomEyesLayer.fixedColorGlowing(Color3.parseHex("#f2dc29")),
+                CustomEyesLayer.fixedColorGlowing(Color3.parseHex("#f2dc29")),
+                CustomEyesLayer::noRender, CustomEyesLayer::noRender));
+        this.addLayer(GasMaskLayer.forSnouted(this, context.getModelSet()));
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(LatexDawnWolfEntity entity) {
+        return new ResourceLocation("goodblock:textures/entities/latex/latex_dawn_wolf.png");
+    }
+}
---
+++ src/main/java/net/hhdsj/goodblock/entity/simple/LatexDawnWolfEntity.java
@@ -0,0 +1,35 @@
+package net.hhdsj.goodblock.entity.simple;
+
+import net.foxyas.changedaddon.entity.advanced.AbstractKitsuneEntity;
+import net.hhdsj.goodblock.init.GoodblockModEntities;
+import net.ltxprogrammer.changed.entity.ChangedEntity;
+import net.ltxprogrammer.changed.entity.Gender;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.network.PlayMessages;
+
+import java.util.Objects;
+
+public class LatexDawnWolfEntity extends AbstractKitsuneEntity {
+    @Override
+    protected void setAttributes(AttributeMap attributes) {
+        super.setAttributes(attributes);
+        Objects.requireNonNull(attributes.getInstance(Attributes.MOVEMENT_SPEED)).setBaseValue(1.1);
+        Objects.requireNonNull(attributes.getInstance(ForgeMod.SWIM_SPEED.get())).setBaseValue(1.1);
+    }
+
+    public LatexDawnWolfEntity(EntityType<? extends ChangedEntity> type, Level level) {
+        super(type, level);
+    }
+
+    public LatexDawnWolfEntity(PlayMessages.SpawnEntity ignoredPacket, Level world) {
+        this(GoodblockModEntities.LATEX_DAWN_WOLF.get(), world);
+    }
+
+    public Gender getGender() {
+        return Gender.MALE;
+    }
+}

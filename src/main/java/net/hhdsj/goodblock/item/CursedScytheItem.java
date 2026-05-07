adb433fccdeb1d20d2d01c0d61624515c0f329f8
hhdsjgit <abc18688567431@163.com>
2026-05-07 22:31:57 +0800

    Added cursed_scythe png Added lunar_dawn tool png

---
+++ src/main/java/net/hhdsj/goodblock/item/CursedScytheItem.java
@@ -0,0 +1,76 @@
+package net.hhdsj.goodblock.item;
+
+import net.hhdsj.goodblock.procedures.Crusedscythe1Procedure;
+import net.hhdsj.goodblock.procedures.Crusedscythe2Procedure;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.*;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.network.chat.Component;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class CursedScytheItem extends SwordItem {
+	public CursedScytheItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 5000;
+			}
+
+			public float getSpeed() {
+				return 1f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 25f;
+			}
+
+			public int getLevel() {
+				return 0;
+			}
+
+			public int getEnchantmentValue() {
+				return 14;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.NETHER_STAR));
+			}
+		}, 3, -2.8f, new Item.Properties().rarity(Rarity.EPIC).fireResistant());
+	}
+
+	@Override
+	public boolean hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+		boolean retval = super.hurtEnemy(itemstack, entity, sourceentity);
+		Crusedscythe1Procedure.execute(entity);
+		return retval;
+	}
+
+	@Override
+	public @NotNull InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		Crusedscythe2Procedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), entity, ar.getObject());
+		return ar;
+	}
+
+	@Override
+	public void appendHoverText(@NotNull ItemStack itemstack, Level level, @NotNull List<Component> list, @NotNull TooltipFlag flag) {
+		super.appendHoverText(itemstack, level, list, flag);
+		list.add(Component.translatable("item.goodblock.cursed_scythe.description_0"));
+		list.add(Component.translatable("item.goodblock.cursed_scythe.description_1"));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public boolean isFoil(@NotNull ItemStack itemstack) {
+		return true;
+	}
+}
---
+++ src/main/java/net/hhdsj/goodblock/procedures/Crusedscythe2Procedure.java
@@ -0,0 +1,64 @@
+package net.hhdsj.goodblock.procedures;
+
+import net.hhdsj.goodblock.init.GoodblockModItems;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.util.RandomSource;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.core.BlockPos;
+
+
+
+public class Crusedscythe2Procedure {
+	public static boolean execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack itemstack) {
+		//进行判断实体是否为null
+		if (entity == null) {
+			return false;
+		}
+
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == GoodblockModItems.CURSED_SCYTHE.get() && entity.isShiftKeyDown()) {
+			//给自己伤害15,凋零
+			entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.WITHER)), 15);
+
+			for (Entity entityiterator : world.getEntities(entity, new AABB((x - 7), (y - 7), (z - 7), (x + 7), (y + 7), (z + 7)))) {
+				if (entity instanceof LivingEntity _entity){
+					_entity.swing(InteractionHand.MAIN_HAND, true);
+					}
+				entityiterator.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.WITHER_SKULL)), 80);
+				//在实体上创建闪电实体
+				if (world instanceof ServerLevel _level) {
+					LightningBolt entityToSpawn = EntityType.LIGHTNING_BOLT.create(_level);
+                    if (entityToSpawn != null) {
+                        entityToSpawn.moveTo(Vec3.atBottomCenterOf(BlockPos.containing(entityiterator.getX(), entityiterator.getY(), entityiterator.getZ())));
+                    }
+					//设置为无伤害
+                    entityToSpawn.setVisualOnly(true);
+					//实体生成
+					_level.addFreshEntity(entityToSpawn);
+				}
+				//消耗提供物品的耐久
+				if (itemstack.hurt(1, RandomSource.create(), null)) {
+					itemstack.shrink(1);
+					itemstack.setDamageValue(0);
+				}
+			}
+			//给物品400tick的冷却
+			if (entity instanceof Player _player) {
+				_player.getCooldowns().addCooldown(itemstack.getItem(), 400);
+			}
+		}
+		//成功运行返回true
+		return true;
+	}
+}

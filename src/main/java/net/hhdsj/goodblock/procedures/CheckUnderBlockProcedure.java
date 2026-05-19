799e39db552cc8e3fcf54ed2d093925420143794
hhdsjgit <abc18688567431@163.com>
2026-05-19 22:00:21 +0800

    修改文件名称

--- src/main/java/net/hhdsj/goodblock/procedures/CheckUnderBlockProcedure.java
+++ src/main/java/net/hhdsj/goodblock/procedures/CheckUnderBlockProcedure.java
@@ -5,7 +5,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.core.BlockPos;
 
-public class Paletreespa1DangPiLinFangKuaiGengXinShiProcedure {
+public class CheckUnderBlockProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		if ((world.getBlockState(BlockPos.containing(x, y - 1, z))) == Blocks.AIR.defaultBlockState()) {
 			{
